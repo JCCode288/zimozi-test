46660b310495132ab0fbda39fe534e33761c9af1
jc288 <jendycaprico@gmail.com>
2025-04-03 16:21:37 +0700

    fix(functions): remove express adapter

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,22 +2,15 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import GlobalExceptionHandler from './modules/global/global.exception';
 import { GlobalInterceptor } from './modules/global/global.interceptor';
-import * as express from 'express';
-import { ExpressAdapter } from '@nestjs/platform-express';
 import 'reflect-metadata';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
 let nestServer;
 
 async function bootstrap() {
-  const expressServer = express();
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressServer),
-    {
-      logger: ['warn', 'error', 'fatal', 'log', 'verbose', 'debug'],
-    },
-  );
+  const app = await NestFactory.create(AppModule, {
+    logger: ['warn', 'error', 'fatal', 'log', 'verbose', 'debug'],
+  });
   app.enableCors();
   app.setGlobalPrefix('/api');
 
